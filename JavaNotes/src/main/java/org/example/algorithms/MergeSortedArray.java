41d14405284d0f2b97ea665461aa667496a9d1b8
Mustafa Redifoglu <mustafaredifoglu@gmail.com>
2025-01-19 23:11:01 +0300

    some algorithms

---
+++ JavaNotes/src/main/java/org/example/algorithms/MergeSortedArray.java
@@ -0,0 +1,20 @@
+package org.example.algorithms;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[i - m];
+        }
+        Arrays.sort(nums1);
+    }
+}
--- JavaNotes/src/main/java/org/example/list/methods.java
+++ JavaNotes/src/main/java/org/example/list/methods.java
@@ -81,6 +81,4 @@ public class methods {
         return mergedList;
     }
 
-    
-
 }
